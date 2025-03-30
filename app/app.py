a497600f8d91773c18049f9a974d5cb05b11dac6
Jeová Anderson <jeova.herminio@gmail.com>
2025-03-30 18:46:27 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -31,5 +31,5 @@ registrar_callbacks(app, dados)
 
 if __name__ == '__main__':
     app.run(debug=True)
-     port = int(os.environ.get("PORT", 8050))  # Usa a porta do Heroku ou 8050 como fallback
+    port = int(os.environ.get("PORT", 8050))  # Usa a porta do Heroku ou 8050 como fallback
     app.run(host="0.0.0.0", port=port, debug=False)
